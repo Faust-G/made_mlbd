525233eacedf8835fb2c55f5513cb9e8db797ab5
Viacheslav <slava.barinov2002@yandex.ru>
2022-10-26 01:41:08 +0300

    Add res and code

---
+++ HW1/red.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+import sys
+
+size = 0
+mean = 0
+var = 0
+
+for line in sys.stdin:
+    s, m, v = map(float,line.split())
+    var = (size * var + s * v)/ (size + s) + size * s * ((mean - m) / (size + s)) **2
+    mean = (size * mean + s * m) / (size + s)
+    size += s
+    
+print(mean, var)
+
