563b6556bc6f96f9f5fe4eb40be5ac5a15bccaba
Viacheslav <slava.barinov2002@yandex.ru>
2022-10-25 03:16:54 +0300

    Done scripts

---
+++ HW1/map_mean.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+from csv import reader
+import sys
+
+size = 0
+mean = 0
+var = 0
+for line in reader(sys.stdin):
+    try:
+        price = float(line[9])
+    except:
+        continue
+    var = size * var / (size + 1) + size * ((mean - 1) / (size + 1)) **2
+    mean = (size * mean + price) / (size + 1)
+    size += 1
+print(chunk_size, mean, var)
